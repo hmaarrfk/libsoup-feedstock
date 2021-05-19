158ea7820b06192707807dfa28d0f426eb1076bc
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2021-05-19 12:09:28 +0100

    Add python test

---
+++ recipe/run_test.py
@@ -0,0 +1,25 @@
+import gi
+import os
+import sys
+from pathlib import PurePath
+
+gi.require_version("Soup", "2.4")
+from gi.repository import GObject, Soup, Gio, GLib
+
+msg = Soup.Message.new("GET", "https://conda-forge.org")
+session = Soup.Session.new()
+
+if os.name == 'nt':
+    ca_file = PurePath(os.environ["CONDA_PREFIX"], "Library", "ssl", "cacert.pem")
+    try:
+        db = Gio.TlsFileDatabase.new(str(ca_file))
+    except GLib.Error as e:
+        print(f"Could not create TLS database for {str(ca_file)} -> {e.message}")
+        sys.exit(1)
+    else:
+        session.props.tls_database = db
+        session.props.ssl_use_system_ca_file = False
+
+session.send_message(msg) # blocks
+if msg.props.status_code < 200 or msg.props.status_code >= 300:
+    print(f"Found status code: {msg.props.status_code}")
